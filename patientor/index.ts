94fe0492528e27b3b5b0edf306c69c5657b20a93
Anwar Sh <alvaro.sh03@gmail.com>
2023-09-15 03:25:53 +0500

    Change port; install cors

--- patientor/index.ts
+++ patientor/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import cors from 'cors';
+
 const app = express();
 app.use(express.json());
+app.use(cors());
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.get('/api/ping', (_req, res) => {
     console.log('someone pinged here');
